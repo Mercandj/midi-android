ddeff79ddd2c89deb5143c9268755e26400156d9
Mercandj <modjow.jm9@gmail.com>
2019-01-19 10:13:42 +0100

    [Samples] :wrench: Sender and receiver

---
+++ midi/src/main/java/com/mercandalli/android/sdk/midi/ScopeLogger.java
@@ -0,0 +1,11 @@
+package com.mercandalli.android.sdk.midi;
+
+public interface ScopeLogger {
+
+    /**
+     * Write the text string somewhere that the user can see it.
+     *
+     * @param text
+     */
+    void log(String text);
+}
